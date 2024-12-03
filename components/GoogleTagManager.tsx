b40502257c858e38c502d3c5358ebcf8d03326d4
kelvinguchu <kelvinguchu5@gmail.com>
2024-12-03 10:48:48 +0300

    analytics fix

--- components/GoogleTagManager.tsx
+++ components/GoogleTagManager.tsx
@@ -5,16 +5,21 @@ import Script from 'next/script'
 export default function GoogleTagManager() {
   return (
     <>
+
+      {/* Google Analytics */}
       <Script
-        id="google-tag-manager"
-        strategy="beforeInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=GT-NS4BQ3N"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
       >
         {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-58V89S63');
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'GT-NS4BQ3N');
         `}
       </Script>
     </>
