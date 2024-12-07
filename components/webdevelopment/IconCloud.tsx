1c50b6a7bbe56489cbe28235ba859b0802a719e6
kelvinguchu <kelvinguchu5@gmail.com>
2024-12-07 09:13:56 +0300

    icon link

--- components/webdevelopment/IconCloud.tsx
+++ components/webdevelopment/IconCloud.tsx
@@ -13,20 +13,27 @@ import {
   SiWix,
   SiShopware,
 } from "react-icons/si";
+import Link from 'next/link';
 
 const icons = [
-  { Icon: SiWordpress, name: 'WordPress' },
-  { Icon: SiShopify, name: 'Shopify' },
+  { 
+    Icon: SiWordpress, 
+    name: 'WordPress',
+    href: '/blog/what-is-wordpress-and-how-to-start-using-it-a-beginners-guide'
+  },
+  { 
+    Icon: SiShopify, 
+    name: 'Shopify',
+    href: '/blog/what-is-shopify-a-beginners-guide-for-aspiring-entrepreneurs'
+  },
   { Icon: SiWoo, name: 'WooCommerce' },
   { Icon: SiMagento, name: 'Magento' },
   { Icon: SiDrupal, name: 'Drupal' },
   { Icon: SiJoomla, name: 'Joomla' },
   { Icon: SiSquarespace, name: 'Squarespace' },
   { Icon: SiWebflow, name: 'Webflow' },
-
   { Icon: SiElementor, name: 'Elementor' },
   { Icon: SiWix, name: 'Wix' },
-
 ];
 
 const IconCloud = () => {
@@ -38,10 +45,25 @@ const IconCloud = () => {
             key={index}
             className="flex flex-col items-center justify-center p-6 rounded-xl hover:bg-white/50 transition-all duration-300"
           >
-            <icon.Icon 
-              className="w-16 h-16 text-gray-700 hover:text-[#F56E0F] transition-colors mb-3" 
-            />
-            <span className="text-base text-gray-600 text-center font-medium">{icon.name}</span>
+            {icon.href ? (
+              <Link href={icon.href} className="flex flex-col items-center">
+                <icon.Icon 
+                  className="w-16 h-16 text-gray-700 hover:text-[#F56E0F] transition-colors mb-3" 
+                />
+                <span className="text-base text-gray-600 text-center font-medium hover:text-[#F56E0F]">
+                  {icon.name}
+                </span>
+              </Link>
+            ) : (
+              <>
+                <icon.Icon 
+                  className="w-16 h-16 text-gray-700 hover:text-[#F56E0F] transition-colors mb-3" 
+                />
+                <span className="text-base text-gray-600 text-center font-medium">
+                  {icon.name}
+                </span>
+              </>
+            )}
           </div>
         ))}
       </div>
